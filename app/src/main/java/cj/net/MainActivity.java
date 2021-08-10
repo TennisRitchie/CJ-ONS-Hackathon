8766cc6b3396364a92582e8e34bb4542a0ac38d3
TennisRitchie <dkekd9@naver.com>
2021-08-10 18:00:31 +0900

    Implementation Line Chart & Pie Chart

--- app/src/main/java/cj/net/MainActivity.java
+++ app/src/main/java/cj/net/MainActivity.java
@@ -1,18 +1,31 @@
 package cj.net;
+import android.graphics.Color;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.EventLogTags;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Integer> jsonList = new ArrayList<>(); // ArrayList 선언
@@ -25,46 +38,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String path = System.getProperty("user.dir");
-        System.out.println("Working Directory = " + path);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         readCSV(monitorings,dailies);
-
-
-//        BarChart chart = findViewById(R.id.barchart);
-//        ArrayList NoOfEmp = new ArrayList();
-//        NoOfEmp.add(new BarEntry(945f, 0));
-//        NoOfEmp.add(new BarEntry(1040f, 1));
-//        NoOfEmp.add(new BarEntry(1133f, 2));
-//        NoOfEmp.add(new BarEntry(1240f, 3));
-//        NoOfEmp.add(new BarEntry(1369f, 4));
-//        NoOfEmp.add(new BarEntry(1487f, 5));
-//        NoOfEmp.add(new BarEntry(1501f, 6));
-//        NoOfEmp.add(new BarEntry(1645f, 7));
-//        NoOfEmp.add(new BarEntry(1578f, 8));
-//        NoOfEmp.add(new BarEntry(1695f, 9));
-//
-//        ArrayList year = new ArrayList();
-//        year.add("2008");
-//        year.add("2009");
-//        year.add("2010");
-//        year.add("2011");
-//        year.add("2012");
-//        year.add("2013");
-//        year.add("2014");
-//        year.add("2015");
-//        year.add("2016");
-//        year.add("2017");
-//
-//        BarDataSet bardataset = new BarDataSet(NoOfEmp, "No Of Employee");
-//        chart.animateY(5000);
-//
-//        BarData data = new BarData(year, bardataset); // MPAndroidChart v3.X 오류 발생
-//        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
-//        chart.setData(data);
-
+        harmnessChart(monitorings);
+        monitoringChart(monitorings);
     }
+
     public void readCSV(ArrayList<Monitoring> monitorings,ArrayList<Daily> dailies){
         String line;
         try { // monitoring.csv
@@ -90,4 +70,101 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         }
+
+        // harmness Pie Chart
+        public void harmnessChart(ArrayList<Monitoring> monitorings){
+           PieChart pieChart = findViewById(R.id.harmnesschart);
+           ArrayList HarmnessData = new ArrayList();
+
+           float variable = (float)monitorings.get(monitorings.size()-1).getHarmness();
+
+            HarmnessData.add(new Entry(variable,0));
+            HarmnessData.add(new Entry(100f - variable,1));
+
+           PieDataSet dataSet = new PieDataSet(HarmnessData, "Number Of Employees");
+           ArrayList harmness = new ArrayList();
+           harmness.add("위험 수준");
+           harmness.add("");
+
+           PieData data = new PieData(harmness, dataSet); // MPAndroidChart v3.X 오류 발생
+           pieChart.setData(data);
+           Legend l = pieChart.getLegend();
+           l.setEnabled(false);
+            // description hide
+            pieChart.setDescription(null);
+           dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+           pieChart.animateXY(5000, 5000);
+           pieChart.setHoleRadius(75);
+           // set center Text
+           pieChart.setCenterText("위험도\n" + String.valueOf(Math.round(variable)) + "%\n");
+           pieChart.setCenterTextSize(35);
+           //pieChart.setCenterTextRadiusPercent();
+    }
+
+    // monitoring Line Charts
+    public void monitoringChart(ArrayList<Monitoring> monitorings){
+        LineChart lineChart = findViewById(R.id.monitoringchart);
+        String[] columns= {"심박수","자궁수축율","태동","혈당","혈압"};
+        int[] colors= {Color.BLUE,Color.RED,Color.MAGENTA,Color.YELLOW,Color.GREEN};
+        int index = 0;
+        boolean[] options = new boolean[5];
+        Arrays.fill(options,true);
+        ArrayList<String> xAXES = new ArrayList<>();
+
+        ArrayList<Entry>[] entry = new ArrayList[5];
+        for(int i = 0; i <5;i++) entry[i] = new ArrayList<>();
+        for(Monitoring monitoring : monitorings) {
+            float FHRs = (float) monitoring.getFHR();
+            entry[0].add(new Entry(FHRs,index));
+
+            float UCs = (float) monitoring.getUC();
+            entry[1].add(new Entry(UCs, index));
+
+            float FMs = (float) monitoring.getFM();
+            entry[2].add(new Entry(FMs, index));
+
+            float glucoses = (float) monitoring.getGlucose();
+            entry[3].add(new Entry(glucoses, index));
+
+            float pressures = (float) monitoring.getPressure();
+            entry[4].add(new Entry(pressures, index));
+
+            index++;
+        }
+        ArrayList<ILineDataSet> lineDataSets = new ArrayList<>();
+        LineDataSet lineDataSet1 = new LineDataSet(entry[0], columns[0]);
+        lineDataSet1.setDrawCircles(false);
+        lineDataSet1.setColor(colors[0]);
+
+        LineDataSet lineDataSet2 = new LineDataSet(entry[1], columns[1]);
+        lineDataSet2.setDrawCircles(false);
+        lineDataSet2.setColor(colors[1]);
+
+        LineDataSet lineDataSet3 = new LineDataSet(entry[2], columns[2]);
+        lineDataSet3.setDrawCircles(false);
+        lineDataSet3.setColor(colors[2]);
+
+        LineDataSet lineDataSet4 = new LineDataSet(entry[3], columns[3]);
+        lineDataSet4.setDrawCircles(false);
+        lineDataSet4.setColor(colors[3]);
+
+        LineDataSet lineDataSet5 = new LineDataSet(entry[4], columns[4]);
+        lineDataSet5.setDrawCircles(false);
+        lineDataSet5.setColor(colors[4]);
+
+        lineDataSets.add(lineDataSet1);
+        lineDataSets.add(lineDataSet2);
+        lineDataSets.add(lineDataSet3);
+        lineDataSets.add(lineDataSet4);
+        lineDataSets.add(lineDataSet5);
+
+        String[] x = new String[3194];
+        for(int i = 0; i < 3194; i++){
+            int min = i / 60;
+            int sec = i % 60;
+            x[i] = "12시 "+String.valueOf(min)+"분 "+String.valueOf(sec)+"초";
+        }
+        lineChart.setData(new LineData(x, lineDataSets));
+        lineChart.setDescription(null);
+    }
     }
